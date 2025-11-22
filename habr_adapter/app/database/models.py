36f13c741264580093a17be0df2d2ff7fe08a6dc
Arkadiy Senin <80350988+ddreamboy@users.noreply.github.com>
2025-11-22 17:54:54 +0300

    Обновлен healthcheck для habr-adapter

--- habr_adapter/app/database/models.py
+++ habr_adapter/app/database/models.py
@@ -1,21 +1,28 @@
 from datetime import datetime
+from typing import Optional
 
-from sqlalchemy import JSON, TIMESTAMP, Integer, String, func
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import JSON, TIMESTAMP, String, func
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
 class Base(DeclarativeBase):
     __abstract__ = True
 
-    id: int = Integer(primary_key=True, autoincrement=True)
-    created_at: datetime = TIMESTAMP(server_default=func.now())
-    updated_at: datetime = TIMESTAMP(
-        server_default=func.now(), onupdate=func.now()
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    created_at: Mapped[datetime] = mapped_column(
+        TIMESTAMP, server_default=func.now()
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        TIMESTAMP,
+        server_default=func.now(),
+        onupdate=func.now(),
     )
 
 
 class Article(Base):
     __tablename__ = "articles"
 
-    url: str = String(unique=True, index=True, nullable=False)
-    parsed_content: dict = JSON(nullable=True)
+    url: Mapped[str] = mapped_column(
+        String, unique=True, index=True, nullable=False
+    )
+    parsed_content: Mapped[Optional[dict]] = mapped_column(JSON, nullable=True)
